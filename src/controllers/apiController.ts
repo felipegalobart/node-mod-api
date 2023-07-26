72b60f7aea6cf0b9a66a27c0b4594d6479587bcf
Felipe Galobart Jora <felipe@mitsuwa.com.br>
2023-07-26 15:33:13 -0300

    finalizaçao do projeto

--- src/controllers/apiController.ts
+++ src/controllers/apiController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
-
-import { Phrase } from "../models/phrase";
+import { Sequelize } from "sequelize";
+import { Phrase } from "../models/Phrase";
 
 export const ping = (req: Request, res: Response) => {
   res.json({pong: true});
@@ -14,4 +14,62 @@ export const random = (req: Request, res: Response) => {
 export const nome = (req: Request, res: Response) => {
   let nome: string = req.params.nome;
   res.json({nome});
+}
+
+export const createPhrase = async (req: Request, res: Response) => {
+
+  let author: string = req.body.author;
+  let txt: string = req.body.txt;
+  let newPhrase = await Phrase.create({
+    author,
+    txt
+  })
+  res.status(201);
+  res.json({ id: newPhrase.id, author, txt });
+}
+
+export const listPhrase = async (req: Request, res: Response) => {
+  let list = await Phrase.findAll();
+  res.json({ list });
+}
+
+export const getPhrase = async (req: Request, res: Response) => {
+  let phrase = await Phrase.findByPk(req.params.id);
+  
+  if (phrase) {
+    res.json({ phrase });
+  } else {
+    res.json({Erro: 'Não foi encontrado a frase com esse ID'})
+  }
+}
+
+export const updatePhrase = async (req: Request, res: Response) => {
+  let phrase = await Phrase.findByPk(req.params.id);
+  let { author, txt } = req.body;
+
+  if (phrase) {
+    phrase.author = author;
+    phrase.txt = txt;
+    await phrase.save();
+
+    res.json({ phrase })
+  } else {
+    res.json({Erro: 'Frase não encontrada'});
+  }
+}
+
+export const deletePhrase = async (req: Request, res: Response) => {
+  let { id } = req.params;
+  await Phrase.destroy({ where: {id} });
+  res.json({});
+}
+
+export const randomPhrase = async (req: Request, res: Response) => {
+  let phrase = await Phrase.findOne({
+    order: [
+      Sequelize.fn('RANDOM')
+    ]
+  });
+
+  res.json({phrase});
 }
--- src/routes/api.ts
+++ src/routes/api.ts
@@ -8,5 +8,12 @@ router.get("/ping", ApiController.ping);
 router.get('/random', ApiController.random);
 router.get('/nome/:nome', ApiController.nome);
 
+router.post('/frases', ApiController.createPhrase);
+router.get('/frases', ApiController.listPhrase);
+router.get('/frase/aleatoria', ApiController.randomPhrase);
+router.get('/frase/:id', ApiController.getPhrase);
+router.put('/frase/:id', ApiController.updatePhrase);
+router.delete('/frase/:id', ApiController.deletePhrase);
+
 
 export default router;
